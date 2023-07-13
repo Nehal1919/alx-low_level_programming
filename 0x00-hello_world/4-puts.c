0d24ce17bb6072ad44602832efd4e33e954e25f0
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-13 11:35:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+* main - started
+* Description: c c print without printf not puts
+* Return: 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
