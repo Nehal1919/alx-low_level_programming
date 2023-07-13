56f940010492dc0d6620b0fedf1c953a172357f8
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-13 12:52:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int a;
 long int l;
 long long int ll;
 float f;
-printf("The size of a char: %d byte(s)\n", sizeof(c));
-printf("The size of an int: %d byte(s)\n", sizeof(a));
-printf("The size of a long int: %d byte(s)\n", sizeof(l));
-printf("The size of a long long int: %d byte(s)\n", sizeof(ll));
-printf("The size of a float: %d byte(s)\n", sizeof(f));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(a));
+printf("Size of a long int: %d byte(s)\n", sizeof(l));
+printf("Size of a long long int: %d byte(s)\n", sizeof(ll));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
 
