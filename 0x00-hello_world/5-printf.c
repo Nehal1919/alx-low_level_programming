17fc782c87173e39bae456c4d16e045d42be7056
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-13 12:04:07 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - started
+* Description: c c print with printf not puts
+* Return: 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
