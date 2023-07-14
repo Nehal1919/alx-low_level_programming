55559e3d5be7f7faad4e7f9134b208b19d694c93
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-14 13:09:16 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - started
+* Description: c print numbers from0to9
+* Return: 0 (success)
+*/
+int main(void)
+{
+int a;
+a = 0;
+while (a < 10)
+{
+printf("%d", a);
+a++;
+}
+printf("\n");
+return (0);
+}
+
