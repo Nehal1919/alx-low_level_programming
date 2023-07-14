7a1ef7de53d773c515981ebb63aa46c26e2a8e58
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-14 13:03:15 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - started
+* Description: c print the alphabet in lowercase
+* Return: 0 (success)
+*/
+int main(void)
+{
+char a;
+a = 'a';
+while (a <= 'z')
+{
+if (a != 'e' && a != 'q' && a <= 'z')
+{
+putchar(a);
+}
+a++;
+}
+putchar('\n');
+return (0);
+}
+
