b5ef79c46432c11c0a5ed73961255348df169697
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-14 16:04:52 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - started
+* Description: c prints base 16 in lowercase
+* Return: 0 (success)
+*/
+int main(void)
+{
+int a;
+char c;
+a = 0;
+c = 'a';
+while (a <= 10)
+{
+putchar(a + '0');
+a++;
+}
+while (c <= 'f')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
+
