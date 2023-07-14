eca41644caf4b54af0d3df19750df0d18ec7abcb
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-14 07:46:00 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - started
+* Description: c print positive or negitve or zero
+* Return: 0 (success)
+*/
+int main(void)
+{
+int n;
+if (n > 0)
+{
+printf("%d is positive\n", n); }
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
+
