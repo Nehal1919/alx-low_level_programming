1462d7829e330ba28948403cb272ed92ea6b8561
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-18 16:53:27 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *  @n: the number will be checked
+ *  Return: 1 if n is greater than zero or 0 if n is zero or -1 otherwise
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+-putchar(45);
+return (-1);
+}
+else
+{
+-putchar(0);
+return (0);
+}
+}
