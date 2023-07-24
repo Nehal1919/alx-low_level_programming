f682f06e7381662a3fe9b6964d2154d3265af82a
Nehal1919 <nihal_20225246@fci.helwan.edu.eg>
2023-07-24 08:24:00 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string, starting with char0
+ * @str: the input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i = i + 2;
+}
+_putchar('\n');
+}
